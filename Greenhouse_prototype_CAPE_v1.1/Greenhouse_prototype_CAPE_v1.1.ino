2de3680b5dfb045664f732a60076827b653587d6
LoupHC <loup.hebert.chartrand@gmail.com>
2017-12-10 15:17:07 -0500

    Update Greenhouse_prototype_CAPE_v1.1.ino

--- Greenhouse_prototype_CAPE_v1.1/Greenhouse_prototype_CAPE_v1.1.ino
+++ Greenhouse_prototype_CAPE_v1.1/Greenhouse_prototype_CAPE_v1.1.ino
@@ -24,240 +24,20 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/*******************************************************
-/*******************CONTROL PARAMETERS******************
-/*******************************************************
-
-/********************Timepoints parameters***************
-
-Timepoints define the greenhouse normal temperature range over time. To set a timepoint, four parameters are asked:
-- type (timpoint can be set relatively to sunrise or sunset, or set manually)
-- time (if set relatively to sunrise or sunset, the time parameter is a value between -60 minuts and +60 minuts,.
-        if set manually, the time parameter is two values : an hour value and a minut value)
-- heating temperature (reference temperature for heating devices)
-- cooling temperature (reference temperature for cooling devices(rollups included)
-
-SYNTAX RULES:
-- type variable(TYPE) : SR, CLOCK and SS (sunrise, manual, sunset)
-- hour variable(HOUR) : 0 for SR/SS types 
-                  0-24 for CLOCK type
-- min variable(MN_MOD) :  -60 to 60 for SR/SS types
-                  0 to 60 for CLOCK type
-- heating temperature variable(HEAT) : 0-50
-- cooling temperature variable(COOL) : 0-50
-
-EXAMPLE 1 (timepoint relative to sunrise): 
-
-#define TP1_TYPE        SR
-#define TP1_HOUR        0
-#define TP1_MN_MOD      -30
-#define TP1_HEAT        18
-#define TP1_COOL        20  
-Timepoint occur 30 minuts before sunrise, heating temperature reference is set to 18C, cooling temperature reference is set to 20C.
-
-EXAMPLE 2 (timepoint relative to sunrise): 
-
-#define TP1_TYPE        CLOCK
-#define TP1_HOUR        12
-#define TP1_MN_MOD      30
-#define TP1_HEAT        20
-#define TP1_COOL        25  
-Timepoint occur at 12h30, heating temperature reference is set to 20C, cooling temperature reference is set to 25C.
-
-/********************Rollups parameters***************
- 
-Rollup parameters set the general behaviour of the roll-up motors, according to measured temperature and cooling reference temperature
-A rollup program splits in two parts : global parameters and stages parameters 
-- Global parameters are active at all time
-- Stages parameters are only active within a short temperature range, defined as "stage" or "cool stage". 
-  They set the target increment (%) within this temperature range
-
-For global parameters, four parameters are asked:
-- hysteresis (tolerated temperature drop before closing)
-- rotation up (# of sec before rollup reaches the top)
-- rotation down (# of sec before rollup reaches the bottom)
-- pause time(pause (in sec) between each motor move)
-
-SYNTAX RULES :
-- hysteresis (HYST): 0 to 5
-- rotation up(ROTUP): 0 to 300
-- rotation down (ROTDOWN): 0 to 300
-- pause time(PAUSE): 0 to 240
-
-For each stages parameters (there is usally many stages), two parameters are asked :
-- temperature modificator (Adds to cooling reference temperature, defines at which temperature the "cool stage" starts)
-- target increments (while in this stage, rollup will move to reach this increment, in % of opening.
-
-SYNTAX RULES :
-- temperature mod(MOD) : -5 to 10
-- target increment(TARGET): 0 to 100
-
-EXAMPLE :
-#define R1_HYST         1
-#define R1_ROTUP        189
-#define R1_ROTDOWN      150
-#define R1_PAUSE        30
-
-#define R1_S1_MOD       0 
-#define R1_S1_TARGET    25
-#define R1_S2_MOD       1
-#define R1_S2_TARGET    50
-#define R1_S3_MOD       2
-#define R1_S3_TARGET    75
-#define R1_S4_MOD       3
-#define R1_S4_TARGET    100
-
-Total rotation time is 189 seconds, total closing time is 150 seconds.
-At cooling temperature + 0C, rollup will open at 25%. At cooling temperature +0(mod) -1(hysteresis), it will close back to 0%.
-At cooling temperature + 1C, rollup will open at 50%. At cooling temperature +1(mod) -1(hysteresis), it will close back to 25%(last stage target target increment).
-At cooling temperature + 2C, rollup will open at 75%. At cooling temperature +2(mod) -1(hysteresis), it will close back to 50%(last stage target target increment).
-At cooling temperature + 3C, rollup will open at 100%. At cooling temperature +3(mod) -1(hysteresis), it will close back to 75%(last stage target target increment).
-
-If cooling temperature is 24C : 
-At 24C, rollup will open at 25%. At 23C, it will close back to 0%.
-At 25C, rollup will open at 50%. At 24C, it will close back to 25%.
-At 26C, rollup will open at 75%. At 25C, it will close back to 50%.
-At 27C, rollup will open at 100%. At 25C, it will close back to 75%.
-*/
-
-/********************Fans/heaters parameters***************
-
-Fan parameters set the general behaviour of ON/OFF cooling devices (typically, fans), according to measured temperature and cooling reference temperature
-Two parameters are asked :
-- hysteresis (tolerated temperature drop before shutting)
-- temperature modificator (Adds to cooling reference temperature, defines at which temperature it starts)
-
-SYNTAX RULES:
-- hysteresis : 0 to 5
-- temperature modificator : -5 to 10
-
-Example : 
-#define F1_MOD          3
-#define F1_HYST         1
-At cooling reference +3, fan will starts.
-At cooling reference temperature +3 (mod) -1 (hyst), fan will stops.
-
-If cooling reference temperature is 24C :
-At 27C, fan will starts.
-At 26C, fan will stops.
- 
-Heater parameters set the general behaviour of ON/OFF heating devices (typically, heaters), according to measured temperature and heating reference temperature
-- hysteresis (tolerated temperature rise before shutting)
-- temperature modificator (Substract to heating reference temperature, defines at which temperature it starts)
-
-SYNTAX RULES:
-- hysteresis : 0 to 5
-- temperature modificator : -10 to 5
-
-EXAMPLE :
-Example : 
-#define H1_MOD          -1
-#define H1_HYST         2
-At heating reference -1, furnace will start.
-At heating reference temperature +-1 (mod) +1 (hyst), furnace will stop.
-
-If cooling reference temperature is 18C :
-At 17C, furnace will start.
-At 19C, furnace will stop.
-*/ 
-
-//************************************************************
-//*******************CONTROL PARAMETERS***********************
-//************************************************************
- 
-//********************Geographic/time parameters***************
-#define TIMEZONE      -5
-#define LATITUDE      45.50
-#define LONGITUDE    -73.56
-
-//#define RTC_TIME_SET
-#define HOUR_SET       21
-#define MINUT_SET      6
-#define SECOND_SET     30
-
-//#define RTC_DATE_SET
-#define DAY_SET        8
-#define MONTH_SET      12
-#define YEAR_SET       2017
-//*******************************************************Timepoint 1
-#define TP1_TYPE        SR
-#define TP1_HOUR        0
-#define TP1_MN_MOD      -30
-#define TP1_HEAT        18
-#define TP1_COOL        20   
-//*******************************************************Timepoint 2
-#define TP2_TYPE        SR
-#define TP2_HOUR        0
-#define TP2_MN_MOD      0
-#define TP2_HEAT        18
-#define TP2_COOL        22
-//*******************************************************Timepoint 3
-#define TP3_TYPE        CLOCK
-#define TP3_HOUR        12
-#define TP3_MN_MOD      30
-#define TP3_HEAT        20
-#define TP3_COOL        24
-//*******************************************************Timepoint 4
-#define TP4_TYPE        SS
-#define TP4_HOUR        0
-#define TP4_MN_MOD      -60
-#define TP4_HEAT        16
-#define TP4_COOL        18
-//*******************************************************Timepoint 5
-#define TP5_TYPE        SS
-#define TP5_HOUR        0
-#define TP5_MN_MOD      0
-#define TP5_HEAT        17
-#define TP5_COOL        19
-//*******************************************************************
-//*******************************************************Rollup 1 (overral parameters)
-#define R1_HYST         1
-#define R1_ROTUP        189
-#define R1_ROTDOWN      150
-#define R1_PAUSE        30
-//*******************************************************Rollup 1 (stages)
-#define R1_S1_MOD       0 
-#define R1_S1_TARGET    25
-#define R1_S2_MOD       1
-#define R1_S2_TARGET    50
-#define R1_S3_MOD       2
-#define R1_S3_TARGET    75
-#define R1_S4_MOD       3
-#define R1_S4_TARGET    100
-//*******************************************************Rollup 2 (overral parameters)
-#define R2_HYST         1
-#define R2_ROTUP        189
-#define R2_ROTDOWN      150
-#define R2_PAUSE        30
-//*******************************************************Rollup 2 (stages)
-#define R2_S1_MOD       0
-#define R2_S1_TARGET    10
-#define R2_S2_MOD       1
-#define R2_S2_TARGET    30
-#define R2_S3_MOD       2
-#define R2_S3_TARGET    50
-#define R2_S4_MOD       3
-#define R2_S4_TARGET    70
-//*************************************************************************
-//*******************************************************Fan parameters
-#define F1_MOD          3
-#define F1_HYST         1
-//**********************************************************************
-//*******************************************************Heater parameters
-#define H1_MOD          -1
-#define H1_HYST         2
-//************************************************************************
-
 //***************************************************************************
 //***************************************************************************
 //***************************************************************************
 /* 
  *                                WARNING!!!
  *  
- * CHANGING ANYTHING BELOW THIS LINE COULD HAVE WEIRD EFFECT ON YOUR 
+ * CHANGING ANYTHING BELOW THIS LINE COULD HAVE UNSUSPECTED EFFECTS ON YOUR 
  * CONTROLLER'S BEHAVIOUR. 
- * SHOULDN'T STOP YOU FROM HAVING A LOOK THO! IT'S WHERE THE REAL
- * MAGIC ACTUALLY TAKE PLACE!!
+ * CHECK "Greenhouse_parameters.h" TO CHANGE GREENHOUSE'S SETTINGS.
+ * 
+ * HAVE A LOOK THO! IT'S WHERE THE REAL MAGIC ACTUALLY TAKE PLACE!!
+ * IF YOU MAKE ANY IMPROVEMENTS TO THIS CODE THAT COULD BENEFIT OTHERS, AND 
+ * WISH TO SHARE IT, YOU CAN CONTACT ME AT : loup.hebert.chartrand@gmail.com 
+ *
  * 
 */
 //***************************************************************************
@@ -267,15 +47,12 @@ At 19C, furnace will stop.
 #include "Arduino.h"
 
 //********************LIBRARIES**************************
+
+#include "Greenhouse_parameters.h"
 #include "GreenhouseLib.h"
-#include "Keypad.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <DS3231.h>
-#include <Wire.h>
-#include <LCD.h>
-#include <LiquidCrystal_I2C.h>
-
 
 //********************PINOUT**************************
 #define WEST_OPENING_PIN 4 //connect this pin to the opening relay (west motor)
@@ -286,21 +63,7 @@ At 19C, furnace will stop.
 #define HEATER_PIN       3 //connect this pin to the heater relay
 #define ONE_WIRE_BUS     A1 //connect this pin to the DS18B20 data line
 
-
-
-//********************OBJECTS**************************
-// Create lcd object using NewLiquidCrystal lib
-#define I2C_ADDR    0x27              // Define I2C Address where the PCF8574A is
-#define BACKLIGHT_PIN     3
-LiquidCrystal_I2C  lcd(I2C_ADDR, 2, 1, 0, 4, 5, 6, 7);
-
-//Create DS18B20 object
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
-//Create a RTC object
-DS3231  rtc(SDA, SCL);                // Init the DS3231 using the hardware interface
-Time  t;
+//********************GREENHOUSE**************************
 
 //Create greenhouse object
 const byte TIMEPOINTS = 5;          //# of timepoints 
@@ -323,10 +86,28 @@ Rollup &rollup2 = greenhouse.rollup[1];
 Fan &fan1 = greenhouse.fan[0];
 Heater &heater1 = greenhouse.heater[0];
 
+//********************INPUT**************************
+
+//Create DS18B20 object
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+//Create a RTC object
+DS3231  rtc(SDA, SCL);                // Init the DS3231 using the hardware interface
+Time  t;
+
 //********************VARIABLES**************************
 
+//Time array
 byte rightNow[6];
+//Temperature inside the greenhouse
 float greenhouseTemperature;
+boolean sensorFailure = false;
+//********************INTERFACE**************************
+
+//See "Greenhouse_interface.h" for LCD display functions
+#include "Greenhouse_interface.h"
+
 
 //***************************************************
 //********************SETUP**************************
@@ -350,7 +131,7 @@ void setup() {
     rtc.setDate(DAY_SET, MONTH_SET, YEAR_SET);
   #endif
   //get RTC values
-  getDateAndTime();                             //get RTC values
+  getDateAndTime();
   //set time within greenhouse object
   greenhouse.setNow(rightNow);
   //calculate hour saving, sunrise, sunset
@@ -366,25 +147,17 @@ void setup() {
   //********************Rollup 1 (east)*******************
   rollup1.initOutputs(MAN_TEMP, ACT_HIGH, EAST_OPENING_PIN, EAST_CLOSING_PIN);
   rollup1.setParameters(0, R1_HYST, R1_ROTUP, R1_ROTDOWN, 100, R1_PAUSE, true);
-  rollup1.setStageMod(1,R1_S1_MOD);
-  rollup1.setStageTarget(1,R1_S1_TARGET);
-  rollup1.setStageMod(2,R1_S2_MOD);
-  rollup1.setStageTarget(2,R1_S2_TARGET);
-  rollup1.setStageMod(3,R1_S3_MOD);
-  rollup1.setStageTarget(3,R1_S3_TARGET);
-  rollup1.setStageMod(4,R1_S4_MOD);
-  rollup1.setStageTarget(4,R1_S4_TARGET);
+  rollup1.setStageParameters(1,R1_S1_MOD, R1_S1_TARGET);
+  rollup1.setStageParameters(2,R1_S2_MOD, R1_S2_TARGET);
+  rollup1.setStageParameters(3,R1_S3_MOD, R1_S3_TARGET);
+  rollup1.setStageParameters(4,R1_S4_MOD, R1_S4_TARGET);
   //********************Rollup 2 (west)*******************
   rollup2.initOutputs(MAN_TEMP, ACT_HIGH, WEST_OPENING_PIN, WEST_CLOSING_PIN); 
   rollup2.setParameters(0, R2_HYST, R2_ROTUP, R2_ROTDOWN, 100, R2_PAUSE, true); 
-  rollup2.setStageMod(1,R2_S1_MOD);
-  rollup2.setStageTarget(1,R2_S1_TARGET);
-  rollup2.setStageMod(2,R2_S2_MOD);
-  rollup2.setStageTarget(2,R2_S2_TARGET);
-  rollup2.setStageMod(3,R2_S3_MOD);
-  rollup2.setStageTarget(3,R2_S3_TARGET);
-  rollup2.setStageMod(4,R2_S4_MOD);
-  rollup2.setStageTarget(4,R2_S4_TARGET);
+  rollup2.setStageParameters(1,R2_S1_MOD, R2_S1_TARGET);
+  rollup2.setStageParameters(2,R2_S2_MOD, R2_S2_TARGET);
+  rollup2.setStageParameters(3,R2_S3_MOD, R2_S3_TARGET);
+  rollup2.setStageParameters(4,R2_S4_MOD, R2_S4_TARGET);
   //*************************Fan**************************
   fan1.initOutput(VAR_TEMP, FAN_PIN);
   fan1.setParameters(F1_MOD, F1_HYST, true);
@@ -404,10 +177,12 @@ void setup() {
 //***************************************************
 
 void loop() {
+  Serial.print("Failure : ");
+  Serial.println(sensorFailure);
   //actual time
   getDateAndTime();
   //actual temperature
-  greenhouseTemperature = getGreenhouseTemp();
+  getGreenhouseTemp();
   //diplay infos on LCD screen
   lcdDisplay();
   //timepoint and target temperatures definitions, outputs routine
@@ -439,167 +214,18 @@ void getDateAndTime(){
 }
 
 
-float getGreenhouseTemp(){
+void getGreenhouseTemp(){
     sensors.requestTemperatures();
     float temp = sensors.getTempCByIndex(0);
-    if((temp < -20.00)||(temp > 80)){
+    
+    if((temp == -127.00)||(temp == 85.00)){
       temp = greenhouse._coolingTemp+10;
-      return temp;
-    }
-    return temp;
-}
-
-byte onebit[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B10000,
-};
-byte twobit[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B10100,
-};byte threebit[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B10101,
-};
-byte bitCount1 = 0;
-byte bitCount2 = 0;
-
-void initLCD(byte length, byte width){
-  lcd.begin(length, width);
-  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
-  lcd.setBacklight(HIGH);
-  lcd.createChar(0, onebit);
-  lcd.createChar(1, twobit);
-  lcd.createChar(2, threebit);
-  lcd.clear();
-}
-
-
-void lcdDisplay(){
-  lcdPrintTemp();
-  lcdPrintTempCible();
-  lcdPrintRollups();
-  lcdPrintOutputs();
-  lcdPrintTime();
-}
-
-void lcdPrintOutputs(){
-    //Fan
-    lcd.setCursor(0, 3); lcd.print(F("          "));
-    lcd.setCursor(0, 3); lcd.print(F("FAN: "));
-    if(digitalRead(FAN_PIN) == HIGH){
-      lcd.print("ON");
+      greenhouseTemperature = temp;
+      sensorFailure = true;
     }
     else{
-      lcd.print("OFF");
+      greenhouseTemperature = temp;
+      sensorFailure = false;
     }
-    //Heater
-    lcd.setCursor(9, 3); lcd.print(F("          "));
-    lcd.setCursor(9, 3); lcd.print(F("|HEAT: "));
-    if(digitalRead(HEATER_PIN) == HIGH){
-      lcd.print("ON");
-    }
-    else{
-      lcd.print("OFF");
-    }
-}
-void lcdPrintRollups(){
-    //East rollup
-    lcd.setCursor(0, 2); lcd.print(F("          "));
-    lcd.setCursor(0, 2); lcd.print(F("EAST:"));
-    if(rollup1.incrementCounter() == OFF_VAL){  
-      lcd.setCursor(0, 2);lcd.print(F(""));
-    }
-    else if (rollup1.opening() == true){
-      lcd.setCursor(0, 2); lcd.print("OPENING  ");
-    }
-    else if (rollup1.closing() == true){
-      lcd.setCursor(0, 2); lcd.print("CLOSING  ");
-    }
-    else{
-      lcd.print(rollup1.incrementCounter());lcd.print("%");
-    }
-    if(((rollup1.closing() == false)&&(rollup1.closingCycle() == true))||((rollup1.opening() == false)&&(rollup1.openingCycle() == true))){
-      if(bitCount1 == 0){
-        lcd.write(byte(0));
-        bitCount1 ++;
-      }
-      else if(bitCount1 == 1){
-        lcd.write(byte(1));
-        bitCount1 ++;
-      }
-      else if(bitCount1 == 2){
-        lcd.write(byte(2));
-        bitCount1 = 0;
-      }
-    }
-    //West rollup
-    lcd.setCursor(9, 2); lcd.print(F("          "));
-      lcd.setCursor(9, 2); lcd.print(F("|WEST:"));
-    if(rollup2.incrementCounter() == OFF_VAL){  
-      lcd.print(F(""));
-    }
-    else if (rollup2.opening() == true){
-      lcd.setCursor(9, 2); lcd.print("|OPENING   ");
-    }
-    else if (rollup2.closing() == true){
-      lcd.setCursor(9, 2); lcd.print("|CLOSING   ");
-    }
-    else{
-      lcd.print(rollup2.incrementCounter());lcd.print("%");
-    }
-    if(((rollup2.closing() == false)&&(rollup2.closingCycle() == true))||((rollup2.opening() == false)&&(rollup2.openingCycle() == true))){
-      if(bitCount2 == 0){
-        lcd.write(byte(0));
-        bitCount2 ++;
-      }
-      else if(bitCount2 == 1){
-        lcd.write(byte(1));
-        bitCount2 ++;
-      }
-      else if(bitCount2 == 2){
-        lcd.write(byte(2));
-        bitCount2 = 0;
-      }
-    }
-}
-void lcdPrintTemp(){
-    float temperature = greenhouseTemperature;
-    lcd.setCursor(0,0); lcd.print(F("         "));
-    lcd.setCursor(0,0); lcd.print(temperature); lcd.print(F("C"));
 }
 
-void lcdPrintTempCible(){
-      lcd.setCursor(9,0);lcd.print("|TT:");lcd.print((short)(greenhouse._heatingTemp + rollup1.tempParameter())); lcd.print(F("-"));lcd.print((short)(greenhouse._coolingTemp + rollup1.tempParameter())); lcd.print(F("C"));
-}
-void lcdPrintTime(){
-    lcd.setCursor(0,1); lcdPrintDigits(rightNow[HOUR]); lcd.print(F(":")); lcdPrintDigits(rightNow[MINUT]);
-    lcd.setCursor(9,1); lcd.print(F("|Timep. ")), lcd.print(greenhouse._timepoint);
-}
-
-void lcdPrintDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  if(digits < 10)
-  lcd.print("0");
-  lcd.print(digits);
-}
-
-void serialPrintDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  if(digits < 10)
-  Serial.print("0");
-}
